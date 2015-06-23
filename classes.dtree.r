6ca554d675041ccb49bd4661a1efaa45b22cfcc9
Gustavo Sampaio <gbritosampaio@gmail.com>
2015-06-23 01:04:12 -0400

    EOL linux default dtree

---
+++ classes.dtree.r
@@ -0,0 +1,79 @@
+data <- read.csv("data/classes.csv", header=TRUE)
+
+cols <- dim(data)[2]
+rows <- dim(data)[1]
+
+train_data <- data[ 1:(0.15*rows), ]
+test_data <- data[ (0.75*rows):rows, ]
+
+# we are predicting the last attribute
+# neg (0) to the left, pos (1) to the right
+# node: list( left_node, right_node, index )
+
+gen_node <- function(left_node, right_node, index){
+
+  return( list(left_node, right_node, index) )
+
+}
+
+# purity: prob of 1
+calc_purity <- function(cdata, index){
+
+  pred_index <- dim(cdata)[2]
+
+  ldata <- cdata[ cdata[,index]==0 ,]
+  rdata <- cdata[ cdata[,index]==1 ,]
+
+  lpos <- ldata[ ldata[,pred_index]==1 ,]
+  ltotal <- dim(ldata)[1]
+  lpos_total <- dim(lpos)[1]
+
+  rpos <- rdata[ rdata[,pred_index]==1 ,]
+  rtotal <- dim(rdata)[1]
+  rpos_total <- dim(rpos)[1]
+
+  return( (lpos_total/ltotal)^2 + (rpos_total/rtotal)^2 )
+
+}
+
+get_left_leaf <- function(cdata, index){
+  ldata <- cdata[ cdata[,index]==0 ,]
+  return(ldata)
+}
+
+get_right_leaf <- function(cdata, index){
+  rdata <- cdata[ cdata[,index]==1 ,]
+  return(rdata)
+}
+
+gen_tree <- function(cdata, level){
+
+  # print(level)
+
+  crows <- dim(cdata)[1]
+  ccols <- dim(cdata)[2]
+
+  if(ccols==1 || crows==1){
+    return(gen_node(-1,-1, -1))
+  }
+
+  purities <- unlist( lapply( c(1:ccols), function(x) calc_purity(cdata, x) ) )
+
+  # we can use as root
+  pure_node <- which.max( purities[ 1:(length(purities)-1) ] )
+
+  left_cdata <- get_left_leaf(cdata, pure_node)[,-pure_node]
+  level[1]<-level[1]+1
+  left_node <- gen_tree(left_cdata, level)
+
+  right_cdata <- get_right_leaf(cdata, pure_node)[,-pure_node]
+  level[2]<-level[2]+1
+  right_node <- gen_tree(right_cdata, level)
+
+  return(gen_node(left_node, right_node, pure_node))
+
+}
+
+root <- gen_tree(train_data[,2:cols], c(1,1))
+
+print(root)
--- classes.kmeans.r
+++ classes.kmeans.r
@@ -15,22 +15,50 @@ colnames(clusters_census) <- c("Count")
 
 total_dists <- 0
 
-get_dist_cluster <- function(row) {
+get_dist_cluster <- function(nclusters, row) {
 
-  dists <- t( apply( clusters, 1, function(x) (x - row)^2 ) )
+  dists <- t( apply( nclusters, 1, function(x) (x - row)^2 ) )
   dists <- cbind( apply( dists, 1, sum ) )
   dists <- cbind( apply( dists, 1, sqrt ) )
 
   # assigned <- order(dists, decreasing=TRUE)[1]
   assigned <- which.min(dists)
 
-  clusters_census[assigned,1] <- clusters_census[assigned,1]+1
+  clusters_census[assigned,1] <<- clusters_census[assigned,1]+1
 
-  total_dists <- total_dists+dists[assigned,1]
+  total_dists <<- total_dists+dists[assigned,1]
 
 }
 
-a <- t( apply( train_data[,2:cols], 1, function(x) get_dist_cluster(x) ) )
+a <- t( apply( train_data[,2:cols], 1, function(x) get_dist_cluster(clusters, x) ) )
 
-# Evolutionary increase of clusters centers
-# Check the mininum total_dists
+# max_changes <- (2^(cols-1))
+max_changes <- 100000
+
+total_dists_min <- total_dists
+clusters_census_min <- clusters_census
+
+for(i in 1:max_changes){
+
+  new_clusters <- matrix(sample(0:1,k*(cols-1), replace=TRUE),k,cols-1)
+
+  # print(new_clusters)
+
+  total_dists <<- 0
+  clusters_census <<- matrix(0, k, 1)
+  colnames(clusters_census) <- c("Count")
+
+  a <- t( apply( train_data[,2:cols], 1, function(x) get_dist_cluster(new_clusters, x) ) )
+
+  if(total_dists<total_dists_min){
+    clusters <<- new_clusters
+    clusters_census_min <<- clusters_census
+    total_dists_min <<- total_dists
+    print("Min")
+  }
+
+  print(paste("Step ", i, "/", max_changes, "... Dist: ", total_dists, sep=""))
+
+}
+
+print(clusters)
